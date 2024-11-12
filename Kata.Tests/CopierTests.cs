2daa6a80463d5b635506cb51c3b18218d9622dd4
Celumusa Zulu <CelumusaZ@hollywoodbets.net>
2024-11-12 11:16:51 +0200

    chore:  created Copier calss with all the neccessary dependencies and add unit test

---
+++ Kata.Tests/CopierTests.cs
@@ -0,0 +1,41 @@
+﻿using NSubstitute;
+using NUnit.Framework;
+
+namespace Kata.Tests;
+
+public class CopierTests
+{
+    [Test]
+    public void Copy_GivenNoCharsToCopy_ShouldNotCopyAnything()
+    {
+        // Arrange
+        var source = Substitute.For<ISource>();
+        var destination = Substitute.For<IDestination>();
+        var copier = new Copier(source, destination);
+
+        source.ReadChar().Returns('\n');
+
+        // Act
+        copier.Copy();
+        // Assert
+        destination.DidNotReceive().WriteChar(Arg.Any<char>());
+    }    
+}
+
+public class Copier(ISource source, IDestination destination)
+{
+    internal void Copy()
+    {
+        
+    }
+}
+
+public interface ISource
+{
+    char ReadChar();
+}
+
+public interface IDestination
+{
+    void WriteChar(char charToCopy);
+}
