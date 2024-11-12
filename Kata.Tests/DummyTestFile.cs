69ee9cc5c03c796b9a7c1e45be5dd73ca06b6cc0
Celumusa Zulu <CelumusaZ@hollywoodbets.net>
2024-11-12 10:59:18 +0200

    chore: added project dependencies

---
+++ Kata.Tests/DummyTestFile.cs
@@ -0,0 +1,12 @@
+﻿using NUnit.Framework;
+
+namespace Kata.Tests;
+
+public class DummyTestFile
+{
+    [Test]
+    public void Test_GivenDependanciesInstalled_ShouldNotThrow()
+    {
+        Assert.DoesNotThrow(() => { });
+    }
+}
