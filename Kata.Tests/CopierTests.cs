77dbe839e215004620f3e79c21019378250df14e
Celumusa Zulu <CelumusaZ@hollywoodbets.net>
2024-11-12 11:29:49 +0200

    chore: added more test cases - for a case when we only have a newline char

--- Kata.Tests/CopierTests.cs
+++ Kata.Tests/CopierTests.cs
@@ -14,13 +14,31 @@ public class CopierTests
         var destination = Substitute.For<IDestination>();
         var copier = new Copier(source, destination);
 
+        char empty = '\0';
+
+        source.ReadChar().Returns(empty);
+
+        // Act
+        copier.Copy();
+        // Assert
+        destination.DidNotReceive().WriteChar(Arg.Any<char>());
+    }  
+    
+    [Test]
+    public void Copy_GivenSourceContainsOnlyNewLineCahr_ShouldNotWriteAnything()
+    {
+        // Arrange
+        var source = Substitute.For<ISource>();
+        var destination = Substitute.For<IDestination>();
+        var copier = new Copier(source, destination);
+
         source.ReadChar().Returns('\n');
 
         // Act
         copier.Copy();
         // Assert
         destination.DidNotReceive().WriteChar(Arg.Any<char>());
-    }    
+    }
 }
 
 
